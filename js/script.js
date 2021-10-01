deb1f0d3044ecbe2f78683943a98413580695692
aman2909verma <aman.verma@knoldus.com>
2021-10-01 13:08:16 +0530

    add new release #63

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ const issues = [
         "link": "#"
     },
     {
-        "text": "Issue#62",
+        "text": "Issue#63",
         "link": "index.html"
+    },
+    {
+        "text": "Issue#62",
+        "link": "issue_62.html"
     },
     {
         "text": "Issue#61",
